c1c5733aebc34bd539c97b48cd361d0ca2bf736f
js <Jim-shop@users.noreply.github.com>
2023-05-09 21:56:42 +0800

    fix: 依赖清理

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,15 +63,12 @@ android {
  * To learn more, go to Add build dependencies.
  */
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    implementation("androidx.compose.material3:material3:1.0.1")
-    implementation("androidx.compose.material3:material3-android:1.1.0-beta02")
-    implementation("androidx.compose.material3:material3-window-size-class-android:1.1.0-beta02")
+    implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
