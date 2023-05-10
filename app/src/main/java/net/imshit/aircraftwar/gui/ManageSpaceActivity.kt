b0036ebc3b88a57b19083f8df7f6db17be7428a6
js <Jim-shop@users.noreply.github.com>
2023-05-10 20:24:27 +0800

    feat: 管理页面Icon

--- app/src/main/java/net/imshit/aircraftwar/gui/ManageSpaceActivity.kt
+++ app/src/main/java/net/imshit/aircraftwar/gui/ManageSpaceActivity.kt
@@ -2,6 +2,8 @@ package net.imshit.aircraftwar.gui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import net.imshit.aircraftwar.R
 import net.imshit.aircraftwar.databinding.ActivityManageSpaceBinding
 
 class ManageSpaceActivity : AppCompatActivity() {
@@ -9,6 +11,20 @@ class ManageSpaceActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         with(ActivityManageSpaceBinding.inflate(layoutInflater)) {
             setContentView(root)
+
+            amsTb.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.item_about ->
+                        MaterialAlertDialogBuilder(this@ManageSpaceActivity)
+                            .setTitle(R.string.item_about)
+                            .setIcon(R.drawable.ic_about_24)
+                            .setMessage(R.string.app_about)
+                            .setPositiveButton(android.R.string.ok) { _, _ -> }
+                            .show()
+                }
+                return@setOnMenuItemClickListener true
+            }
+
             // TODO 有了存储逻辑之后记得加入
         }
     }
