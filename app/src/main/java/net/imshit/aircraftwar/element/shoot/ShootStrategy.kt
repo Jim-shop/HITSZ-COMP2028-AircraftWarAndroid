2674accf98f0f48e975bddc7161217510b92c674
js <Jim-shop@users.noreply.github.com>
2023-05-11 05:54:37 +0800

    refactor: 重命名AbstractBullet -> Bullets

--- app/src/main/java/net/imshit/aircraftwar/element/shoot/ShootStrategy.kt
+++ app/src/main/java/net/imshit/aircraftwar/element/shoot/ShootStrategy.kt
@@ -1,8 +1,8 @@
 package net.imshit.aircraftwar.element.shoot
 
-import net.imshit.aircraftwar.element.bullet.AbstractBullet
+import net.imshit.aircraftwar.element.bullet.Bullets
 import net.imshit.aircraftwar.logic.Games
 
 interface ShootStrategy {
-    fun shoot(game: Games, x: Float, y: Float, speedY: Float, power: Int): List<AbstractBullet>
+    fun shoot(game: Games, x: Float, y: Float, speedY: Float, power: Int): List<Bullets>
 }
