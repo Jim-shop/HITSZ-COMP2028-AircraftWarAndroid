63fd8d2ee0933a0da4fb918d8335a5e2032302ff
js <Jim-shop@users.noreply.github.com>
2023-05-11 05:04:49 +0800

    feat: 添加ImageManager

---
+++ app/src/main/java/net/imshit/aircraftwar/util/ImageManager.kt
@@ -0,0 +1,24 @@
+package net.imshit.aircraftwar.util
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+
+class ImageManager(context: Context) {
+    val readImage: (String) -> Bitmap = { fileName ->
+        BitmapFactory.decodeStream(context.assets.open(fileName))
+    }
+
+    val backgroundEasy: Bitmap = readImage("game_bg_easy.webp")
+    val backgroundMedium: Bitmap = readImage("game_bg_medium.webp")
+    val backgroundHard: Bitmap = readImage("game_bg_hard.webp")
+    val aircraftHero: Bitmap = readImage("game_aircraft_hero.png")
+    val aircraftMob: Bitmap = readImage("game_aircraft_mob.png")
+    val aircraftElite: Bitmap = readImage("game_aircraft_elite.png")
+    val aircraftBoss: Bitmap = readImage("game_aircraft_boss.png")
+    val bulletHero: Bitmap = readImage("game_bullet_hero.png")
+    val bulletEnemy: Bitmap = readImage("game_bullet_enemy.png")
+    val propBlood: Bitmap = readImage("game_prop_blood.png")
+    val propBomb: Bitmap = readImage("game_prop_bomb.png")
+    val propBullet: Bitmap = readImage("game_prop_bullet.png")
+}
