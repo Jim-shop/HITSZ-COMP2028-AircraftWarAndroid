9b01769ea858d54d9ac647ec0620930e5f7b0339
js <Jim-shop@users.noreply.github.com>
2023-05-10 23:04:02 +0800

    feat: GameActivity载入Game

--- app/src/main/java/net/imshit/aircraftwar/gui/GameActivity.kt
+++ app/src/main/java/net/imshit/aircraftwar/gui/GameActivity.kt
@@ -4,8 +4,15 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import net.imshit.aircraftwar.Difficulty
 import net.imshit.aircraftwar.databinding.ActivityGameBinding
+import net.imshit.aircraftwar.logic.AbstractGame
+import net.imshit.aircraftwar.logic.EasyGame
+import net.imshit.aircraftwar.logic.HardGame
+import net.imshit.aircraftwar.logic.MediumGame
 
 class GameActivity : AppCompatActivity() {
     companion object {
@@ -25,21 +32,35 @@ class GameActivity : AppCompatActivity() {
     val refreshInterval = 10
 
     var gameMode = Difficulty.EASY
+        private set
     var soundMode = true
+        private set
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 获取配置
         with(windowManager.currentWindowMetrics.bounds) {
             width = right - left
             height = bottom - top
         }
+        intent.apply {
+            gameMode = getSerializableExtra("gameMode", Difficulty::class.java) ?: Difficulty.EASY
+            soundMode = getBooleanExtra("soundMode", true)
+        }
+        val game: AbstractGame = when (gameMode) {
+            Difficulty.EASY -> EasyGame(this)
+            Difficulty.MEDIUM -> MediumGame(this)
+            Difficulty.HARD -> HardGame(this)
+        }
         with(ActivityGameBinding.inflate(layoutInflater)) {
-            setContentView(root)
-            // handle input string
-            intent.extras?.apply {
-                gameMode = getSerializable("gameMode", Difficulty::class.java) ?: Difficulty.EASY
-                soundMode = getBoolean("soundMode", true)
-            }
+            setContentView(root.apply {
+                addView(game, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT))
+            })
+        }
+        // 隐藏任务栏、导航栏
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            hide(WindowInsetsCompat.Type.systemBars())
         }
     }
 }
