62de334a287757e9dab78e270e6b4aee383e87b2
js <Jim-shop@users.noreply.github.com>
2023-05-12 02:18:11 +0800

    refactor: 更新掉落物速度

--- app/src/main/java/net/imshit/aircraftwar/element/prop/factory/PropFactories.kt
+++ app/src/main/java/net/imshit/aircraftwar/element/prop/factory/PropFactories.kt
@@ -5,7 +5,7 @@ import net.imshit.aircraftwar.logic.game.Games
 
 abstract class PropFactories(val game: Games) {
     val speedX = 0f
-    val speedY = 0.1f
+    val speedY = 0.2f
 
     abstract fun createProp(x: Float, y: Float): Props?
 }
