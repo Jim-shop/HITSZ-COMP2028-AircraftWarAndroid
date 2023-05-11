ec29c7eda1fbbb8191aba0b3001948d3ded096e1
js <Jim-shop@users.noreply.github.com>
2023-05-11 20:58:53 +0800

    refactor: ImageManager 收敛权限

--- app/src/main/java/net/imshit/aircraftwar/logic/ImageManager.kt
+++ app/src/main/java/net/imshit/aircraftwar/logic/ImageManager.kt
@@ -6,23 +6,23 @@ import android.graphics.BitmapFactory
 import android.graphics.Matrix
 
 class ImageManager(context: Context, width: Int) {
-    val scaleMatrix = (width / 512f).let { Matrix().apply { postScale(it, it) } }
-    val readImage: (String) -> Bitmap = { fileName ->
+    private val scaleMatrix = (width / 512f).let { Matrix().apply { postScale(it, it) } }
+    private val readImage: (String) -> Bitmap = { fileName ->
         BitmapFactory.decodeStream(context.assets.open("img/$fileName")).run {
             Bitmap.createBitmap(this, 0, 0, this.width, this.height, scaleMatrix, true)
         }
     }
 
-    val backgroundEasy: Bitmap = readImage("game_bg_easy.webp")
-    val backgroundMedium: Bitmap = readImage("game_bg_medium.webp")
-    val backgroundHard: Bitmap = readImage("game_bg_hard.webp")
-    val aircraftHero: Bitmap = readImage("game_aircraft_hero.png")
-    val aircraftMob: Bitmap = readImage("game_aircraft_mob.png")
-    val aircraftElite: Bitmap = readImage("game_aircraft_elite.png")
-    val aircraftBoss: Bitmap = readImage("game_aircraft_boss.png")
-    val bulletHero: Bitmap = readImage("game_bullet_hero.png")
-    val bulletEnemy: Bitmap = readImage("game_bullet_enemy.png")
-    val propBlood: Bitmap = readImage("game_prop_blood.png")
-    val propBomb: Bitmap = readImage("game_prop_bomb.png")
-    val propBullet: Bitmap = readImage("game_prop_bullet.png")
+    val backgroundEasy = readImage("game_bg_easy.webp")
+    val backgroundMedium = readImage("game_bg_medium.webp")
+    val backgroundHard = readImage("game_bg_hard.webp")
+    val aircraftHero = readImage("game_aircraft_hero.png")
+    val aircraftMob = readImage("game_aircraft_mob.png")
+    val aircraftElite = readImage("game_aircraft_elite.png")
+    val aircraftBoss = readImage("game_aircraft_boss.png")
+    val bulletHero = readImage("game_bullet_hero.png")
+    val bulletEnemy = readImage("game_bullet_enemy.png")
+    val propBlood = readImage("game_prop_blood.png")
+    val propBomb = readImage("game_prop_bomb.png")
+    val propBullet = readImage("game_prop_bullet.png")
 }
