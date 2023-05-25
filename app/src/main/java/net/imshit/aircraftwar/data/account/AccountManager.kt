43255b76ff9457c7eb403e403ae8c9293a9bcf6d
js <Jim-shop@users.noreply.github.com>
2023-05-25 21:22:27 +0800

    feat: 登录界面

--- app/src/main/java/net/imshit/aircraftwar/data/account/AccountManager.kt
+++ app/src/main/java/net/imshit/aircraftwar/data/account/AccountManager.kt
@@ -2,29 +2,46 @@ package net.imshit.aircraftwar.data.account
 
 import android.content.Context
 import android.content.DialogInterface
+import android.view.LayoutInflater
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import net.imshit.aircraftwar.R
+import net.imshit.aircraftwar.databinding.DialogLoginBinding
+import java.net.URL
 
 object AccountManager {
-    fun showLoginDialog(context: Context, onSuccess: Runnable) {
-        val dialogListener = DialogInterface.OnClickListener { _, which ->
-            when (which) {
-                DialogInterface.BUTTON_NEGATIVE -> {}
-                DialogInterface.BUTTON_POSITIVE -> {
-                    // TODO()
-                    onSuccess.run()
-                }
+    private const val SALT = "HITsz520"
+    private val REMOTE = URL("https://haxiaoshen.top/")
 
-                DialogInterface.BUTTON_NEUTRAL -> {
-                    // TODO()
-                    onSuccess.run()
+    fun login(account: String, password: String, onSuccess: Runnable) {
+        // TODO
+        onSuccess.run()
+    }
+
+    fun register(account: String, password: String, onSuccess: Runnable) {
+        // TODO
+        login(account, password, onSuccess)
+    }
+
+    fun showLoginDialog(context: Context, onSuccess: Runnable) {
+        with(MaterialAlertDialogBuilder(context)) {
+            with(DialogLoginBinding.inflate(LayoutInflater.from(context), null, false)) {
+                val dialogListener = DialogInterface.OnClickListener { _, which ->
+                    when (which) {
+                        DialogInterface.BUTTON_POSITIVE -> ::login
+                        DialogInterface.BUTTON_NEUTRAL -> ::register
+                        else -> null
+                    }?.invoke(
+                        dlTietAc.text.toString(), dlTietPw.text.toString(), onSuccess
+                    )
                 }
+                setView(root)
+                setTitle(R.string.dialog_login_title)
+                setIcon(R.drawable.ic_login_24)
+                setPositiveButton(android.R.string.ok, dialogListener)
+                setNeutralButton(R.string.dialog_login_button_register, dialogListener)
+                setNegativeButton(android.R.string.cancel, dialogListener)
+                show()
             }
         }
-        MaterialAlertDialogBuilder(context).setTitle(R.string.dialog_login_title)
-            .setIcon(R.drawable.ic_login_24).setPositiveButton(android.R.string.ok, dialogListener)
-            .setNeutralButton(R.string.dialog_login_button_register, dialogListener)
-            .setNegativeButton(android.R.string.cancel, dialogListener)
-            .setView(R.layout.dialog_login).show()
     }
 }
